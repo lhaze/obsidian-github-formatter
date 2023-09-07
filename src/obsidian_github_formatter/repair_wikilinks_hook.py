b3e7c38593020ba19de59420f60abbcc74580b42
lhaze <lhaze@lhaze.name>
2023-09-08 01:35:42 +0200

    [refactor] Renaming `links` -> `repair-wikilinks` hook

--- src/obsidian_github_formatter/repair_wikilinks_hook.py
+++ src/obsidian_github_formatter/repair_wikilinks_hook.py
@@ -10,8 +10,10 @@ from .links import process_file
 
 
 @click.command()
-@click.option("--dry-run", is_flag=True, help="Show what would be changed and do not modify any files.")
-@click.option("--make-backups", is_flag=True, help="Copies the backfile before changes are to be mmade.")
+@click.help_option("--help", "-h")
+@click.version_option()
+@click.option("--dry-run", "-n", is_flag=True, help="Show what would be changed and do not modify any files.")
+@click.option("--make-backups", "-b", is_flag=True, help="Copies the backfile before changes are to be mmade.")
 @click.option(
     "--root",
     type=click.Path(exists=True, dir_okay=True, file_okay=False),
@@ -20,6 +22,15 @@ from .links import process_file
 )
 @click.argument("filenames", nargs=-1, type=click.Path(exists=True))
 def main(dry_run: bool, make_backups: bool, root: str, filenames: t.List[str]) -> None:  # pragma: no cover
+    """Repairs wikilinks -- changes [[link]] link format to [link](link).
+
+
+    * Supports titles and spaces in link.
+
+    * Checks whether linked file exists.
+
+    * Supports submodules: changes [[link]] to a link [link](https://raw.githubusercontent.com/.../link), based on `.gitmodules` config file.
+    """
     cache = Cache(
         root=root,
         dry_run=dry_run,
